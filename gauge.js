d00b51ed5971293246d72ad455c21dd2ff431ea5
Eran Goldin <eran.goldin@gmail.com>
2019-10-27 10:48:32 +0200

    Create instance of Gauge

--- gauge.js
+++ gauge.js
@@ -1,14 +1,42 @@
+function drawChart(element) {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Label', 'Value'],
+    ['Rank', 20 - 3]
+  ]);
+  var options = {
+    width: 250,
+    height: 250,
+    redFrom: 0,
+    redTo: 10,
+    yellowFrom: 10,
+    yellowTo: 15,
+    greenFrom: 15,
+    greenTo: 20,
+    minorTicks: 20,
+    max: 20,
+    min: 0,
+    majorTicks: ['20', '1']
+  };
+
+  var chart = new google.visualization.Gauge(element);
+  chart.draw(data, options);
+}
+
 looker.plugins.visualizations.add({
 	create: function(element, config){
 		element.innerHTML = "<h1>Ready to render!</h1>";
+		
+		google.charts.load('current', {'packages':['gauge']});
+		google.charts.setOnLoadCallback(drawChart(element));
 	},
 	updateAsync: function(data, element, config, queryResponse, details, doneRendering){
-		var html = "";
-		for(var row of data) {
-			var cell = row[queryResponse.fields.dimensions[0].name];
-			html += LookerCharts.Utils.htmlForCell(cell);
-		}
-		element.innerHTML = html;
-		doneRendering()
+// 		var html = "";
+// 		for(var row of data) {
+// 			var cell = row[queryResponse.fields.dimensions[0].name];
+// 			html += LookerCharts.Utils.htmlForCell(cell);
+// 		}
+// 		element.innerHTML = html;
+// 		doneRendering()
 	}
 });
