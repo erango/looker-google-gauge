254f6f56e40f6ac96442e54cbe4d7638c8860cb7
Eran Goldin <eran.goldin@gmail.com>
2019-10-27 11:42:06 +0200

    Update gauge.js
    Updated sample code

--- gauge.js
+++ gauge.js
@@ -1,34 +1,33 @@
-function drawChart(element) {
+google.charts.load('current', {'packages':['gauge']});
 
-  var data = google.visualization.arrayToDataTable([
-    ['Label', 'Value'],
-    ['Rank', 20 - 3]
-  ]);
-  var options = {
-    width: 250,
-    height: 250,
-    redFrom: 0,
-    redTo: 10,
-    yellowFrom: 10,
-    yellowTo: 15,
-    greenFrom: 15,
-    greenTo: 20,
-    minorTicks: 20,
-    max: 20,
-    min: 0,
-    majorTicks: ['20', '1']
-  };
+var chart;
 
-  var chart = new google.visualization.Gauge(element);
-  chart.draw(data, options);
+function drawChart() {
+
+	var data = google.visualization.arrayToDataTable([
+		['Label', 'Value'],
+		['Memory', 80],
+		['CPU', 55],
+		['Network', 68]
+	]);
+
+	var options = {
+		width: 400, height: 120,
+		redFrom: 90, redTo: 100,
+		yellowFrom:75, yellowTo: 90,
+		minorTicks: 5
+	};
+
+	chart = new google.visualization.Gauge(document.getElementById('vis'));
+
+	chart.draw(data, options);
 }
 
 looker.plugins.visualizations.add({
 	create: function(element, config){
 		element.innerHTML = "<h1>Ready to render!</h1>";
 		
-		google.charts.load('current', {'packages':['gauge']});
-		google.charts.setOnLoadCallback(drawChart(element));
+		google.charts.setOnLoadCallback(drawChart);
 	},
 	updateAsync: function(data, element, config, queryResponse, details, doneRendering){
 // 		var html = "";
